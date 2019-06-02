5d47f7b38c89e970ad17ed0cc40ef8e2537a8c25
Averiser <averiser@gmail.com>
2019-06-02 17:22:11 +0300

    3rd commit-initial page added

---
+++ English Test Level Check/Controller/WelcomePage.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomePage.swift
+//  English Test Level Check
+//
+//  Created by MyMacBook on 02/06/2019.
+//  Copyright © 2019 MyMacBook. All rights reserved.
+//
+
+import UIKit
+
+class WelcomePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
